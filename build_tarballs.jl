114d469437384cfe0bbf271c1e700e05c2488cfa
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-06-27 15:00:24 +0200

    Improve comments on versions

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,35 +1,35 @@
 using BinaryBuilder, Pkg
 
 name = "DssOpt"
-version = v"1.0.1"  # fake version number
+version = v"1.0.1"
 
 # url = "https://github.com/marcom/dss-opt/"
 # description = ""
 
 sources = [
-    # v1.0 (2012) (release tarballs made on github Sep 22, 2022)
-    # ArchiveSource("https://github.com/marcom/dss-opt/archive/refs/tags/v1.0.tar.gz",
-    #               "16447960e2218d588070208812dcfec3b5be120f70b62c940f79315738601235"),
+    # # v1.0 (2012) (release tarballs made on github Sep 22, 2022)
     # GitSource("https://github.com/marcom/dss-opt/",
     #           "c0c1e0f47f5346453a9e73f26ba08a6e826d5a9b")
 
-    # # 2022-09-23 git-sha1: cb69a3b9af1befdaad3bae53c764594011418016 [DssOpt-v1.0.1+0]
-    # ArchiveSource("https://github.com/marcom/dss-opt/archive/cb69a3b9af1befdaad3bae53c764594011418016.tar.gz",
-    #               "f17a2e2eefa34fbd0e2a31bed20af921b3095163ac7131c1e6457d68139e9057"),
-    GitSource("https://github.com/marcom/dss-opt/",
-              "cb69a3b9af1befdaad3bae53c764594011418016")
+    # # v1.0.1 (2022-09-23) git-sha1: cb69a3b9af1befdaad3bae53c764594011418016
+    # #                     DssOpt-v1.0.1+0 in https://github.com/marcom/DssOpt_jll.jl
+    # GitSource("https://github.com/marcom/dss-opt/",
+    #           "cb69a3b9af1befdaad3bae53c764594011418016")
 
     # # 2022-12-03 git-sha1: 818cf83319ed85e11c01d00db3eeaedd2ce5815f
-    # ArchiveSource("https://github.com/marcom/dss-opt/archive/818cf83319ed85e11c01d00db3eeaedd2ce5815f.tar.gz",
-    #               "4d65a4e995f83a1d70829956517a225ea15a07c2e49f9e27f158399b5492c975"),
     # GitSource("https://github.com/marcom/dss-opt/",
     #           "818cf83319ed85e11c01d00db3eeaedd2ce5815f")
-
-    #DirectorySource("/home/mcm/src/dss-opt/dss-opt.git/"; target="dss-opt-git"),
 
     # 2023-03-19
     # GitSource("https://github.com/marcom/dss-opt/",
     #           "23f785736951e7369ae122a4a1c397948daecea1")
+
+    # 2023-06-26
+    GitSource("https://github.com/marcom/dss-opt/",
+              "dc2af7474fe512e1cb783dead584c3ccbb990de5")
+
+    #DirectorySource("/home/mcm/src/dss-opt/dss-opt.git/";
+    #                target="dss-opt-git"),
 ]
 
 script = raw"""
